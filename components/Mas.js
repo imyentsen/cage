bfb3193e607a0bf74217bce1f78f46919d59882b
劉彥岑 <ansinlau@liuyancens-MacBook-Pro.local>
2022-04-26 23:39:22 +0200

    first post test

--- components/Mas.js
+++ components/Mas.js
@@ -1,9 +1,9 @@
 
 const Mas = ({ title, slogan }) => {
   return (
-    <div className="p-20 text-center font-bold">
-      <p className="text-3xl">{title}</p>
-      <p className="text-3xl">{slogan}</p>
+    <div className="p-12 text-center font-bold">
+      <p className="my-12 text-3xl">{title}</p>
+      <p className="my-6 text-3xl">{slogan}</p>
     </div>
   )
 }
