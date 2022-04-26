1dbcc01e7dc40c741ed70b13427ea738aeb89288
劉彥岑 <ansinlau@liuyancens-MacBook-Pro.local>
2022-04-27 00:03:42 +0200

    first img txt

--- components/BlogPost.js
+++ components/BlogPost.js
@@ -20,6 +20,7 @@ const BlogPost = ({ post }) => {
             <p className="hidden md:block leading-8 text-gray-700 dark:text-gray-300">
               {post.summary}
             </p>
+            <p>{post.image}</p>
             <Image
               src={post.image}
               alt="Picture of the author"
