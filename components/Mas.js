0d5a818cdf678524c235d0a8ae681dc80c6a2409
劉彥岑 <ansinlau@liuyancens-MacBook-Pro.local>
2022-04-26 19:06:10 +0200

    Mas neeeewdd

--- components/Mas.js
+++ components/Mas.js
@@ -10,18 +10,7 @@ const Mas = ({ fullWidth }) => {
         !fullWidth ? 'max-w-2xl px-4' : 'px-4 md:px-24'
       }`}
     >
-
-    <div
-      className={`mt-6 flex-shrink-0 m-auto w-full text-gray-500 transition-all max-w-2xl px-4`}
-    >
-      <div className="my-4 text-sm leading-6">
-        <div className="flex align-baseline justify-between flex-wrap">
-            <h1 className="text-2xl text-center">The Cage Revisited</h1>
-            <h1 className="text-2xl text-center">An inforgraph a day opens your mind on key issues </h1>
-        </div>
-      </div>
-      <hr className="border-gray-200 dark:border-gray-600" />
-    </div>
+      
       <hr className="border-gray-200 dark:border-gray-600" />
       <div className="my-4 text-sm leading-6">
         <div className="flex align-baseline justify-between flex-wrap">
