b33a86b77db0ac57c31f1f71760c8e865e276da0
劉彥岑 <ansinlau@liuyancens-MacBook-Pro.local>
2022-04-26 18:15:10 +0200

    header

--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,6 @@
 import { useEffect, useRef } from 'react'
 import Link from 'next/link'
+import Image from 'next/image'
 import BLOG from '@/blog.config'
 import { useLocale } from '@/lib/locale'
 
@@ -68,6 +69,7 @@ const Header = ({ navBarTitle, fullWidth }) => {
           <Link href="/">
             <a aria-label={BLOG.title}>
               <div className="h-6">
+                <Image src="./public/favicon.svg" height={30} width={30} />
               </div>
             </a>
           </Link>
