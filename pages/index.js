22edc2d41f25696f1325dd2e08bdd32273172e3e
劉彥岑 <ansinlau@liuyancens-MacBook-Pro.local>
2022-04-26 23:40:58 +0200

    first post tests

--- pages/index.js
+++ pages/index.js
@@ -24,7 +24,7 @@ const blog = ({ postsToShow, page, showNext }) => {
   return (
     <Container title={BLOG.title} description={BLOG.description}>
       <Mas title="The Cage Revisited" slogan="An inforgraph a day opens your mind on key issues"/>
-      {postsToShow[0](post => (
+      {postsToShow.map(post => (
         <BlogPost key={post.id} post={post} />
       ))}
       {showNext && <Pagination page={page} showNext={showNext} />}
