2cdf3cfbc61555d37b2b5d2fac5b58921c914e60
劉彥岑 <ansinlau@liuyancens-MacBook-Pro.local>
2022-04-26 19:42:26 +0200

    Mas Text style

--- components/Mas.js
+++ components/Mas.js
@@ -2,6 +2,7 @@ const Mas = ({ slogan }) => {
   return (
     <div className="w-full py-8 text-2xl text-center">
       <p>{slogan}</p>
+      <hr className="border-gray-200 dark:border-gray-600" />
     </div>
   )
 }
