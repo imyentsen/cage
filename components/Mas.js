9b156a52864b474e99c2065f781efabfcf9de594
劉彥岑 <ansinlau@liuyancens-MacBook-Pro.local>
2022-04-26 19:07:50 +0200

    Mas new

--- components/Mas.js
+++ components/Mas.js
@@ -10,7 +10,6 @@ const Mas = ({ fullWidth }) => {
         !fullWidth ? 'max-w-2xl px-4' : 'px-4 md:px-24'
       }`}
     >
-      
       <hr className="border-gray-200 dark:border-gray-600" />
       <div className="my-4 text-sm leading-6">
         <div className="flex align-baseline justify-between flex-wrap">
