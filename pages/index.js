73c9156b326fd5f2ab7f364f7df82b250606e1fa
劉彥岑 <ansinlau@liuyancens-MacBook-Pro.local>
2022-04-27 16:49:42 +0200

    first date

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import Mas from '@/components/Mas'
 import Pagination from '@/components/Pagination'
 import { getAllPosts } from '@/lib/notion'
 import BLOG from '@/blog.config'
+import formatDate from '@/lib/formatDate'
 
 export async function getStaticProps () {
   const posts = await getAllPosts({ includePages: false })
