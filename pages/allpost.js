88545cb40136b6862989d986a6f857c6bffaf292
劉彥岑 <ansinlau@liuyancens-MacBook-Pro.local>
2022-04-28 20:21:18 +0200

    all post

--- pages/allpost.js
+++ pages/allpost.js
@@ -4,7 +4,6 @@ import Mas from '@/components/Mas'
 import Pagination from '@/components/Pagination'
 import { getAllPosts } from '@/lib/notion'
 import BLOG from '@/blog.config'
-import Link from 'next/link'
 import formatDate from '@/lib/formatDate'
 
 export async function getStaticProps () {
@@ -22,21 +21,22 @@ export async function getStaticProps () {
   }
 }
 
-const blog = ({ postsToShow, page, showNext }) => {
+const allpost = ({ postsToShow, page, showNext }) => {
   console.log(postsToShow[0])
   return (
     <Container title={BLOG.title} description={BLOG.description}>
       <Mas title="The Cage Revisited" slogan="An inforgraph a day opens your mind on key issues"/>
+      <div className='text-3xl my-8'>
+        <p>{formatDate(postsToShow[0].date?.start_date || postsToShow[0].createdTime, BLOG.lang)}</p>
+        <p>{postsToShow[0].title}</p>
+        <p className="md:block leading-8 text-gray-700">{postsToShow[0].summary}</p>
+      </div>
       {postsToShow.slice(1).map(post => (
         <BlogPost key={post.id} post={post} />
       ))}
       {showNext && <Pagination page={page} showNext={showNext} />}
-      
-      <Link href="/allpost">
-        <a>TEST</a>
-      </Link>
     </Container>
   )
 }
 
-export default blog
+export default allpost
