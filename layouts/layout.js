cc993e36db6b00400b9aaad5fbde4bb2bb5bec4b
劉彥岑 <ansinlau@liuyancens-MacBook-Pro.local>
2022-04-29 01:16:08 +0200

    avatar

--- layouts/layout.js
+++ layouts/layout.js
@@ -36,19 +36,6 @@ const Layout = ({
         </h1>
         {frontMatter.type[0] !== 'Page' && (
           <nav className="flex mt-7 items-start text-gray-500 dark:text-gray-400">
-            <div className="flex mb-4">
-              <a href={BLOG.socialLink || '#'} className="flex">
-                <Image
-                  alt={BLOG.author}
-                  width={24}
-                  height={24}
-                  src={`https://gravatar.com/avatar/${emailHash}`}
-                  className="rounded-full"
-                />
-                <p className="ml-2 md:block">{BLOG.author}</p>
-              </a>
-              <span className="block">&nbsp;/&nbsp;</span>
-            </div>
             <div className="mr-2 mb-4 md:ml-0">
               {formatDate(
                 frontMatter?.date?.start_date || frontMatter.createdTime,
