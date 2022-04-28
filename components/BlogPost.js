60fd0ee2217892423d588827352f14e78ac50e0d
劉彥岑 <ansinlau@liuyancens-MacBook-Pro.local>
2022-04-28 20:44:08 +0200

    img

--- components/BlogPost.js
+++ components/BlogPost.js
@@ -24,6 +24,8 @@ const BlogPost = ({ post }) => {
               <Image
                 src={post.image}
                 alt="infograph of the day"
+                width={500} 
+                height={500} 
               />
               </div>
           </main>
