af1e5b874b1137449e29ea7a5467192e89e7729d
劉彥岑 <ansinlau@liuyancens-MacBook-Pro.local>
2022-04-28 20:34:17 +0200

    pagination 1

--- pages/index.js
+++ pages/index.js
@@ -7,9 +7,9 @@ import BLOG from '@/blog.config'
 
 export async function getStaticProps () {
   const posts = await getAllPosts({ includePages: false })
-  const postsToShow = posts.slice(0, BLOG.postsPerPage)
+  const postsToShow = posts.slice(0, 1)
   const totalPosts = posts.length
-  const showNext = totalPosts > BLOG.postsPerPage
+  const showNext = totalPosts > BLOG.1
   return {
     props: {
       page: 1, // current page is 1
@@ -25,7 +25,7 @@ const blog = ({ postsToShow, page, showNext }) => {
   return (
     <Container title={BLOG.title} description={BLOG.description}>
       <Mas title="The Cage Revisited" slogan="An inforgraph a day opens your mind on key issues"/>
-      {postsToShow.slice(1).map(post => (
+      {postsToShow.map(post => (
         <BlogPost key={post.id} post={post} />
       ))}
       {showNext && <Pagination page={page} showNext={showNext} />}
