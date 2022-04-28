09e07fa42890532771b7bae4ece5c07f36e773b4
劉彥岑 <ansinlau@liuyancens-MacBook-Pro.local>
2022-04-29 00:20:40 +0200

    footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,24 +1,68 @@
 import BLOG from '@/blog.config'
 import Vercel from '@/components/Vercel'
+import Image from 'next/image'
+
+const FooterBar = () => {
+  const locale = useLocale()
+  const links = [
+    { id: 0, name: locale.NAV.ABOUT, to: '/about', show: BLOG.showAbout },
+    { id: 1, name: locale.NAV.SEARCH, to: '/search', show: true }
+  ]
+  return (
+    <div className="flex-shrink-0">
+      <ul className="flex flex-row">
+        {links.map(
+          link =>
+            link.show && (
+              <li
+                key={link.id}
+                className="block ml-4 text-black dark:text-gray-50 nav"
+              >
+                <Link href={link.to}>
+                  <a>{link.name}</a>
+                </Link>
+              </li>
+            )
+        )}
+      </ul>
+    </div>
+  )
+}
+
 const Footer = ({ fullWidth }) => {
   const d = new Date()
   const y = d.getFullYear()
   const from = +BLOG.since
   return (
     <div
-      className={`mt-6 flex-shrink-0 m-auto w-full text-gray-500 dark:text-gray-400 transition-all ${
+      className={`mt-12 flex-shrink-0 m-auto w-full flex justify-center text-gray-500 dark:text-gray-400 transition-all ${
         !fullWidth ? 'max-w-2xl px-4' : 'px-4 md:px-24'
       }`}
     >
-      <hr className="border-gray-200 dark:border-gray-600" />
-      <div className="my-4 text-sm leading-6">
-        <div className="flex align-baseline justify-between flex-wrap">
-          <p>
-            © {BLOG.author} {from === y || !from ? y : `${from} - ${y}`}
-          </p>
-          <Vercel />
+      
+      <div>
+          <Link href="/">
+            <a aria-label={BLOG.title}>
+              <Image src="favicon.svg" height={64} width={64} />
+            </a>
+          </Link>
+          {navBarTitle
+            ? (
+            <p className="text-xl font-medium text-day dark:text-night header-name">
+              {navBarTitle}
+            </p>
+              )
+            : (
+            <p className="text-xl font-medium text-day dark:text-night header-name">
+              {BLOG.title}
+            </p>
+              )}
         </div>
-      </div>
+
+        <FooterBar />
+      <p>
+        © {BLOG.author} {from === y || !from ? y : `${from} - ${y}`}
+      </p>
     </div>
   )
 }
