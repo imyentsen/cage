a5ff8db77d68504ca9515701e8405b9ba7b8418d
劉彥岑 <ansinlau@liuyancens-MacBook-Pro.local>
2022-05-15 01:30:57 +0200

    mono

--- components/BlogPost.js
+++ components/BlogPost.js
@@ -4,15 +4,14 @@ import formatDate from '@/lib/formatDate'
 
 const BlogPost = ({ post }) => {
   return (
-      <Link href={`${BLOG.path}/${post.slug}`}>
-        <article key={post.id} className="mb-10 md:mb-12 px-4 py-8 md:p-12 border border-gray-300 hover:border-2 ease-in duration-300 cursor-pointer">
+        <article key={post.id} className="mb-10 md:mb-12 px-4 py-8 md:p-12 border border-gray-300">
           <header className="flex flex-col justify-between cursor-pointer text-black text-center">
             <time className="text-xl mb-2 font-mono">
               {formatDate(post?.date?.start_date || post.createdTime, BLOG.lang)}
             </time>
           </header>
           <main>
-            <h2 className="text-2xl mb-2 text-center">
+            <h2 className="text-2xl mb-2 text-center leading-6">
               {post.title}
             </h2>
             <div className='p-4 my-4 flex justify-center align-center bg-white'>
@@ -24,13 +23,14 @@ const BlogPost = ({ post }) => {
             </div>
             <p className="leading-8 text-gray-700 dark:text-gray-300">
               {post.summary}
+              <Link href={`${BLOG.path}/${post.slug}`}>
                 <a className='underline'>
-                  &nbsp;...More
+                    ...More
                 </a>
+              </Link>
             </p>
           </main>
         </article>
-      </Link>
   )
 }
 
