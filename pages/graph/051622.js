2e4a249104d45a5e4599b636d217f201cb74b7f5
劉彥岑 <ansinlau@liuyancens-MacBook-Pro.local>
2022-05-16 01:06:53 +0200

    graph

--- pages/graph/051622.js
+++ pages/graph/051622.js
@@ -48,22 +48,31 @@ export const data = {
     {
       label: 'USA:BUY',
       data: [1.00, 1.04, 1.08, 1.12, 1.17, 1.21, 1.26, 1.31, 1.36, 1.42, 1.47, 1.53, 1.59, 1.65, 1.72, 1.79, 1.86, 1.93, 2.01, 2.09, 2.17, 2.26, 2.34, 2.44, 2.53],
+      borderColor: '#CB554D',
       backgroundColor: '#CB554D',
       yAxisID: 'y'
     },
     {
       label: 'USA:RENT',
       data: [0.27, 0.27, 0.27, 0.28, 0.29, 0.29, 0.30, 0.31, 0.32, 0.33, 0.35, 0.36, 0.38, 0.40, 0.42, 0.45, 0.48, 0.52, 0.56, 0.60, 0.66, 0.72, 0.79, 0.87, 0.96],
+      borderColor: '#601E21',
       backgroundColor: '#601E21',
       yAxisID: 'y'
-    }
+    },
+    {
+      label: 'NORWAY:BUY',
+      data: [1.00, 1.05, 1.11, 1.17, 1.24, 1.30, 1.37, 1.45, 1.53, 1.61, 1.70, 1.79, 1.89, 1.99, 2.10, 2.21, 2.33, 2.46, 2.59, 2.73, 2.88, 3.04, 3.20, 3.37, 3.56],
+      borderColor: '#3E88C4',
+      backgroundColor: '#3E88C4',
+      yAxisID: 'y'
+    },
   ]
 }
 
 function GRAPH () {
   return (
   <div className="my-auto chart-wrapper bg-white">
-    <Bar options={options} data={data} height={500}/>
+    <Line options={options} data={data} height={500}/>
   </div>
   )
 }
