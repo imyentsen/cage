1ba8a156cc813d15e500e9a84e8ba0c7420a81f7
劉彥岑 <ansinlau@liuyancens-MacBook-Pro.local>
2022-05-29 17:55:51 +0200

    card layout

--- components/BlogPost.js
+++ components/BlogPost.js
@@ -4,7 +4,7 @@ import formatDate from '@/lib/formatDate'
 
 const BlogPost = ({ post }) => {
   return (
-        <article key={post.id} className="mb-0 md:mb-8 px-0 pb-4 border-b-1 border-white md:border md:border-post md:border-2 md:outline md:outline-gray-400 bg-post">
+        <article key={post.id} className="mb-0 md:mb-8 px-0 pb-4 border-b-1 border-white md:border md:border-post md:border-2 md:outline-1 md:outline-gray-400 bg-post">
           <header className="px-4 py-2 flex flex-row justify-between border-b border-white text-white">
             <time className="text-base font-mono uppercase">
               {formatDate(post?.date?.start_date || post.createdTime, BLOG.lang)}
@@ -15,7 +15,7 @@ const BlogPost = ({ post }) => {
           </header>
           <main>
             <div className='p-6 md:p-8'>
-              <h2 className="text-2xl md:text-lg font-bold text-gray-900 font-title text-center leading-6 md:leading-8">
+              <h2 className="text-2xl md:text-lg font-bold text-gray-900 font-title text-center leading-normal">
                 {post.title}
               </h2>
             </div>
@@ -37,7 +37,7 @@ const BlogPost = ({ post }) => {
             <div className='py-2'>
               <p className="text-center">
                 <Link href={`${BLOG.path}/${post.slug}`}>
-                  <button className='border border-white hover:border-2 text-white py-2 px-4'>
+                  <button className='border border-white hover:border-2 font-title font-strong text-white py-2 px-4'>
                       More
                   </button>
                 </Link>
