a734ccdfe798667d0d6816c1f395baad2316bdce
劉彥岑 <ansinlau@liuyancens-MacBook-Pro.local>
2022-05-29 18:07:08 +0200

    card layout

--- components/BlogPost.js
+++ components/BlogPost.js
@@ -4,7 +4,7 @@ import formatDate from '@/lib/formatDate'
 
 const BlogPost = ({ post }) => {
   return (
-        <article key={post.id} className="mb-0 md:mb-8 px-0 pb-4 border-b-1 border-white md:border md:border-post md:border-2 md:outline-1 md:outline-gray-400 bg-post">
+        <article key={post.id} className="mb-0 md:mb-8 px-0 pb-4 border-b border-white md:border md:border-post md:border-2 md:outline md:outline-1 md:outline-offset-0 md:outline-gray-400 bg-post">
           <header className="px-4 py-2 flex flex-row justify-between border-b border-white text-white">
             <time className="text-base font-mono uppercase">
               {formatDate(post?.date?.start_date || post.createdTime, BLOG.lang)}
@@ -15,7 +15,7 @@ const BlogPost = ({ post }) => {
           </header>
           <main>
             <div className='p-6 md:p-8'>
-              <h2 className="text-2xl md:text-lg font-bold text-gray-900 font-title text-center leading-normal">
+              <h2 className="text-3xl md:text-lg font-bold text-gray-900 font-title text-center leading-normal">
                 {post.title}
               </h2>
             </div>
@@ -27,10 +27,10 @@ const BlogPost = ({ post }) => {
               />
             </div>
             <div className='p-4 flex flex-col md:flex-row justify-between '>
-              <p className="py-1 md:p-4 leading-normal text-gray-900 md:flex-grow dark:text-gray-300">
+              <p className="py-1 md:p-2 leading-normal text-lg text-gray-900 md:flex-grow dark:text-gray-300">
                 {post.summary}
               </p>
-              <p className="py-1 md:p-4 leading-relaxed text-gray-900 md:flex-none md:w-56 dark:text-gray-300">
+              <p className="py-1 md:p-2 leading-relaxed text-gray-900 md:flex-none md:w-56 dark:text-gray-300">
                 {post.summarytw}
               </p>
             </div>
