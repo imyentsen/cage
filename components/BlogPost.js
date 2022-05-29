ae8be0759ee1d2c5c5b9ec3e021f197cdee18853
劉彥岑 <ansinlau@liuyancens-MacBook-Pro.local>
2022-05-29 18:16:04 +0200

    card layout

--- components/BlogPost.js
+++ components/BlogPost.js
@@ -4,7 +4,7 @@ import formatDate from '@/lib/formatDate'
 
 const BlogPost = ({ post }) => {
   return (
-        <article key={post.id} className="mb-0 md:mb-8 px-0 pb-4 border-b border-white md:border md:border-post md:border-4 md:outline md:outline-1 md:outline-offset-0 md:outline-gray-400 bg-post">
+        <article key={post.id} className="mb-0 md:mb-8 px-0 pb-4 border-b border-white md:border md:border-post md:border-8 md:outline md:outline-1 md:outline-offset-8 md:outline-gray-400 bg-post">
           <header className="px-4 py-2 flex flex-row justify-between border-b border-white text-white">
             <time className="text-base font-mono uppercase">
               {formatDate(post?.date?.start_date || post.createdTime, BLOG.lang)}
@@ -15,7 +15,7 @@ const BlogPost = ({ post }) => {
           </header>
           <main>
             <div className='p-6 md:p-8'>
-              <h2 className="text-lg md:text-2xl font-bold text-black font-title text-center leading-normal">
+              <h2 className="text-lg md:text-xl font-bold text-black font-title text-center leading-normal">
                 {post.title}
               </h2>
             </div>
