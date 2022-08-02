f19b275b76ac35e79bd78c66b2b9170a6af4b75b
劉彥岑 <ansinlau@liuyancens-MBP.lan>
2022-08-02 22:04:20 +0200

    posytpe

--- components/BlogPost.js
+++ components/BlogPost.js
@@ -43,13 +43,14 @@ const BlogPost = ({ post }) => {
                 </Link>
               </p>
               <p className="text-center self-center">
-                {BLOG.path === 'PostTW' ?
-                  <Link href={`${BLOG.path}/${post.slug}-tw`}>
-                    <button className='border border-white hover:border-2 font-title font-strong text-white py-2 px-4'>
-                      中文
-                    </button>
-                  </Link> : 
-                  'Save'}
+                {BLOG.path === 'PostTW' 
+                  ? <Link href={`${BLOG.path}/${post.slug}-tw`}>
+                      <button className='border border-white hover:border-2 font-title font-strong text-white py-2 px-4'>
+                        中文
+                      </button>
+                    </Link> 
+                  : 'Save'
+                }
               </p>
             </div>
           </main>
