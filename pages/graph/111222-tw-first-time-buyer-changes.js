861d62d603931805fc0d3202dc6c361605a541e2
劉彥岑 <ansinlau@liuyancens-MBP.lan>
2022-11-12 23:03:45 +0100

    1112 chart

---
+++ pages/graph/111222-tw-first-time-buyer-changes.js
@@ -0,0 +1,78 @@
+import React from 'react'
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+} from 'chart.js'
+import { Line } from 'react-chartjs-2'
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+)
+
+export const options = {
+  responsive: true,
+  maintainAspectRatio: false,
+  plugins: {
+    legend: {
+      position: 'top'
+    }
+  },
+  scales: {
+    x: {
+      display: true,
+      title: {
+        display: true,
+        text: 'Year'
+      }
+    },
+    y: {
+      type: 'linear', // only linear but allow scale type registration. This allows extensions to exist solely for log scale for instance
+      position: 'left',
+      ticks: {
+        color: '#121212'
+      },
+      title: {
+        display: true,
+        text: 'Suppose 1 = money required for buying an average house'
+      }
+    }
+  }
+}
+
+const labels = ['2002', '2012', '2022', '2032', '2042']
+
+export const data = {
+  labels,
+  datasets: [
+    {
+      label: '首購族人口數',
+      data: [7392380, 7156745, 6372863, 5073447, 3987642],
+      borderColor: '#B5917375',
+      backgroundColor: '#B59173',
+      hidden: true,
+      yAxisID: 'y'
+    },
+  ]
+}
+
+function GRAPH () {
+  return (
+  <div className="my-auto chart-wrapper bg-white">
+    <Line options={options} data={data} height={500}/>
+  </div>
+  )
+}
+
+export default GRAPH
