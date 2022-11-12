38e7f90231a5882f3e09d805f4f0d6659f59333f
劉彥岑 <ansinlau@liuyancens-MBP.lan>
2022-11-13 00:36:40 +0100

    1113 chart

---
+++ pages/graph/121222-tw-household-changes.js
@@ -0,0 +1,94 @@
+import React from 'react'
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+} from 'chart.js'
+import { Line } from 'react-chartjs-2'
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+)
+
+export const options = {
+  responsive: true,
+  maintainAspectRatio: false,
+  plugins: {
+    legend: {
+      position: 'top'
+    }
+  },
+  scales: {
+    x: {
+      display: true,
+      title: {
+        display: true,
+        text: 'Year'
+      }
+    },
+    y: {
+      type: 'linear', // only linear but allow scale type registration. This allows extensions to exist solely for log scale for instance
+      position: 'left',
+      ticks: {
+        color: '#121212'
+      },
+      title: {
+        display: true,
+        text: 'Suppose 1 = money required for buying an average house'
+      }
+    }
+  }
+}
+
+const labels = ['2001', '2002', '2003', '2004', '2005', '2006', '2007', '2008', '2009', '2010', '2011', '2012', '2013', '2014', '2015', '2016', '2017', '2018', '2019', '2020', '2021', '2022', '2023', '2024', '2025', '2026', '2027', '2028', '2029', '2030', '2031', '2032', '2033', '2034', '2035', '2036', '2037', '2038', '2039', '2040', '2041', '2042', '2043', '2044', '2045', '2046', '2047', '2048', '2049', '2050', '2051', '2052', '2053', '2054', '2055', '2056', '2057', '2058', '2059', '2060', '2061', '2062', '2063', '2064', '2065', '2066', '2067', '2068', '2069', '2070']
+
+export const data = {
+  labels,
+  datasets: [
+    {
+      label: '戶數淨變化',
+      data: [null,'122738', '122149', '132775', '112936', '101879', '117691', '143323', '150062', '131190', '120737', '128671', '99828', '96439', '86279', '92405', '87617', '85477', '98268', '82167', '88205', '83979', '86811', '80909', '78641', '76428', '68888', '64553', '64515', '59634', '52828', '52560', '44789', '39590', '41192', '33155', '25966', '20916', '16208', '7678', '4076', '-4686', '-18319', '-16639', '-25055', '-36976', '-37038', '-48453', '-54775', '-54733', '-59869', '-62770', '-68400', '-70180', '-76891', '-73418', '-74112', '-89184', '-79854', '-91291', '-94935', '-91950', '-101700', '-97134', '-107842', '-109118', '-108339', '-111413', '-107555', '-119102'],
+      borderColor: '#B5917375',
+      backgroundColor: '#B59173',
+      hidden: true,
+      yAxisID: 'y'
+    },
+    {
+      label: '全國住宅存量變化',
+      data: [null,null,null,null,null,null,null,null,null,'84374', '98851', '98954', '42371', '89549', '85157', '108414', '94754', '137719', '89410', '45272', '100500',null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null],
+      borderColor: '#3E74C475',
+      backgroundColor: '#3E74C4',
+      hidden: true,
+      yAxisID: 'y'
+    },
+    {
+      label: '新建待售住宅',
+      data: [null,null,null,null,null,null,null,null,null, '25352', '22642', '21945', '25556', '33758', '52149', '68118', '75192', '80082', '78063', '76333', '70049',null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null],
+      borderColor: '#CB554D75',
+      backgroundColor: '#CB554D',
+      hidden: true,
+      yAxisID: 'y'
+    }
+  ]
+}
+
+function GRAPH () {
+  return (
+  <div className="my-auto chart-wrapper bg-white">
+    <Line options={options} data={data} height={500}/>
+  </div>
+  )
+}
+
+export default GRAPH
