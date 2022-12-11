783355bfd68e1701652f84f45750d976d4bd1fed
imyentsen <imyentsen@gmail.com>
2022-12-12 00:44:04 +0100

    order

--- pages/graph/121022-tw-population-changes-cities.js
+++ pages/graph/121022-tw-population-changes-cities.js
@@ -64,14 +64,6 @@ export const data = {
       yAxisID: 'y'
     },
     {
-      label: '宜蘭縣',
-      data: ['3.81', '2.46', '1.10', '0.10', '-1.80'],
-      borderColor: 'rgba(0, 0, 0, 0)',
-      backgroundColor: '#410000',
-      hidden: false,
-      yAxisID: 'y'
-    },
-    {
       label: '新竹縣',
       data: ['10.60', '8.86', '7.33', '5.63', '3.28'],
       borderColor: 'rgba(0, 0, 0, 0)',
@@ -80,66 +72,20 @@ export const data = {
       yAxisID: 'y'
     },
     {
-      label: '苗栗縣',
-      data: ['5.35', '4.16', '3.17', '2.18', '0.71'],
-      borderColor: 'rgba(0, 0, 0, 0)',
-      backgroundColor: '#676767',
-      hidden: false,
-      yAxisID: 'y'
-    },
-    {
-      label: '彰化縣',
-      data: ['2.82', '1.68', '1.03', '0.06', '-1.43'],
+      label: '新北市',
+      data: ['6.35', '5.72', '4.76', '3.55', '2.08'],
       borderColor: 'rgba(0, 0, 0, 0)',
       backgroundColor: '#118607',
       hidden: false,
+      pointStyle: 'rect',
+      usePointStyle: true,
       yAxisID: 'y'
     },
     {
-      label: '南投縣',
-      data: ['1.62', '0.81', '-0.17', '-1.29', '-2.66'],
+      label: '新竹市',
+      data: ['7.99', '6.59', '5.21', '3.66', '1.86'],
       borderColor: 'rgba(0, 0, 0, 0)',
-      backgroundColor: '#7F00AB',
-      hidden: false,
-      yAxisID: 'y'
-    },
-    {
-      label: '雲林縣',
-      data: ['1.21', '0.14', '-0.56', '-1.23', '-2.45'],
-      borderColor: 'rgba(0, 0, 0, 0)',
-      backgroundColor: '#14451E',
-      hidden: false,
-      yAxisID: 'y'
-    },
-    {
-      label: '嘉義縣',
-      data: ['1.01', '0.34', '-0.40', '-0.91', '-2.14'],
-      borderColor: 'rgba(0, 0, 0, 0)',
-      backgroundColor: '#FF6363',
-      hidden: false,
-      yAxisID: 'y'
-    },
-    {
-      label: '屏東縣',
-      data: ['0.81', '-0.40', '-1.35', '-2.48', '-3.93'],
-      borderColor: 'rgba(0, 0, 0, 0)',
-      backgroundColor: '#040A48',
-      hidden: false,
-      yAxisID: 'y'
-    },
-    {
-      label: '臺東縣',
-      data: ['0.03', '-1.21', '-2.62', '-4.15', '-5.62'],
-      borderColor: 'rgba(0, 0, 0, 0)',
-      backgroundColor: '#A7D621',
-      hidden: false,
-      yAxisID: 'y'
-    },
-    {
-      label: '花蓮縣',
-      data: ['1.04', '-0.04', '-1.45', '-2.99', '-4.51'],
-      borderColor: 'rgba(0, 0, 0, 0)',
-      backgroundColor: '#5260E2',
+      backgroundColor: '#C99C06',
       hidden: false,
       yAxisID: 'y'
     },
@@ -152,18 +98,38 @@ export const data = {
       yAxisID: 'y'
     },
     {
-      label: '基隆市',
-      data: ['1.44', '0.45', '-0.80', '-2.18', '-3.79'],
+      label: '桃園市',
+      data: ['8.72', '6.66', '4.68', '3.03', '0.98'],
       borderColor: 'rgba(0, 0, 0, 0)',
-      backgroundColor: '#29DD9C',
+      backgroundColor: '#945D0B',
+      hidden: false,
+      pointStyle: 'rect',
+      usePointStyle: true,
+      yAxisID: 'y'
+    },
+    {
+      label: '苗栗縣',
+      data: ['5.35', '4.16', '3.17', '2.18', '0.71'],
+      borderColor: 'rgba(0, 0, 0, 0)',
+      backgroundColor: '#676767',
       hidden: false,
       yAxisID: 'y'
     },
     {
-      label: '新竹市',
-      data: ['7.99', '6.59', '5.21', '3.66', '1.86'],
+      label: '臺中市',
+      data: ['6.41', '5.12', '3.69', '2.26', '0.69'],
       borderColor: 'rgba(0, 0, 0, 0)',
-      backgroundColor: '#C99C06',
+      backgroundColor: '#BA2909',
+      hidden: false,
+      pointStyle: 'rect',
+      usePointStyle: true,
+      yAxisID: 'y'
+    },
+    {
+      label: '彰化縣',
+      data: ['2.82', '1.68', '1.03', '0.06', '-1.43'],
+      borderColor: 'rgba(0, 0, 0, 0)',
+      backgroundColor: '#118607',
       hidden: false,
       yAxisID: 'y'
     },
@@ -172,6 +138,74 @@ export const data = {
       data: ['2.78', '2.17', '1.05', '-0.10', '-1.49'],
       borderColor: 'rgba(0, 0, 0, 0)',
       backgroundColor: '#D7376F',
+      hidden: false,
+      yAxisID: 'y'
+    },
+    {
+      label: '臺南市',
+      data: ['3.38', '2.41', '1.28', '0.04', '-1.51'],
+      borderColor: 'rgba(0, 0, 0, 0)',
+      backgroundColor: '#1A418D',
+      hidden: false,
+      pointStyle: 'rect',
+      usePointStyle: true,
+      yAxisID: 'y'
+    },
+    {
+      label: '宜蘭縣',
+      data: ['3.81', '2.46', '1.10', '0.10', '-1.80'],
+      borderColor: 'rgba(0, 0, 0, 0)',
+      backgroundColor: '#410000',
+      hidden: false,
+      yAxisID: 'y'
+    },
+    {
+      label: '嘉義縣',
+      data: ['1.01', '0.34', '-0.40', '-0.91', '-2.14'],
+      borderColor: 'rgba(0, 0, 0, 0)',
+      backgroundColor: '#FF6363',
+      hidden: false,
+      yAxisID: 'y'
+    },
+    {
+      label: '高雄市',
+      data: ['3.15', '2.06', '0.75', '-0.68', '-2.34'],
+      borderColor: 'rgba(0, 0, 0, 0)',
+      backgroundColor: '#500E6D',
+      hidden: false,
+      pointStyle: 'rect',
+      usePointStyle: true,
+      yAxisID: 'y'
+    },
+    {
+      label: '雲林縣',
+      data: ['1.21', '0.14', '-0.56', '-1.23', '-2.45'],
+      borderColor: 'rgba(0, 0, 0, 0)',
+      backgroundColor: '#14451E',
+      hidden: false,
+      yAxisID: 'y'
+    },
+    {
+      label: '南投縣',
+      data: ['1.62', '0.81', '-0.17', '-1.29', '-2.66'],
+      borderColor: 'rgba(0, 0, 0, 0)',
+      backgroundColor: '#7F00AB',
+      hidden: false,
+      yAxisID: 'y'
+    },
+    {
+      label: '基隆市',
+      data: ['1.44', '0.45', '-0.80', '-2.18', '-3.79'],
+      borderColor: 'rgba(0, 0, 0, 0)',
+      backgroundColor: '#29DD9C',
+      hidden: false,
+      yAxisID: 'y'
+    },
+    {
+      label: '屏東縣',
+      data: ['0.81', '-0.40', '-1.35', '-2.48', '-3.93'],
+      borderColor: 'rgba(0, 0, 0, 0)',
+      backgroundColor: '#040A48',
       hidden: false,
       yAxisID: 'y'
     },
@@ -186,53 +220,19 @@ export const data = {
       yAxisID: 'y'
     },
     {
-      label: '高雄市',
-      data: ['3.15', '2.06', '0.75', '-0.68', '-2.34'],
+      label: '花蓮縣',
+      data: ['1.04', '-0.04', '-1.45', '-2.99', '-4.51'],
       borderColor: 'rgba(0, 0, 0, 0)',
-      backgroundColor: '#500E6D',
+      backgroundColor: '#5260E2',
       hidden: false,
-      pointStyle: 'rect',
-      usePointStyle: true,
       yAxisID: 'y'
     },
     {
-      label: '新北市',
-      data: ['6.35', '5.72', '4.76', '3.55', '2.08'],
+      label: '臺東縣',
+      data: ['0.03', '-1.21', '-2.62', '-4.15', '-5.62'],
       borderColor: 'rgba(0, 0, 0, 0)',
-      backgroundColor: '#118607',
+      backgroundColor: '#A7D621',
       hidden: false,
-      pointStyle: 'rect',
-      usePointStyle: true,
-      yAxisID: 'y'
-    },
-    {
-      label: '臺中市',
-      data: ['6.41', '5.12', '3.69', '2.26', '0.69'],
-      borderColor: 'rgba(0, 0, 0, 0)',
-      backgroundColor: '#BA2909',
-      hidden: false,
-      pointStyle: 'rect',
-      usePointStyle: true,
-      yAxisID: 'y'
-    },
-    {
-      label: '臺南市',
-      data: ['3.38', '2.41', '1.28', '0.04', '-1.51'],
-      borderColor: 'rgba(0, 0, 0, 0)',
-      backgroundColor: '#1A418D',
-      hidden: false,
-      pointStyle: 'rect',
-      usePointStyle: true,
-      yAxisID: 'y'
-    },
-    {
-      label: '桃園市',
-      data: ['8.72', '6.66', '4.68', '3.03', '0.98'],
-      borderColor: 'rgba(0, 0, 0, 0)',
-      backgroundColor: '#945D0B',
-      hidden: false,
-      pointStyle: 'rect',
-      usePointStyle: true,
       yAxisID: 'y'
     }
   ]
