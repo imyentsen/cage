6878074c5f49d9dfa5363b57ecef038aa8429efc
imyentsen <imyentsen@gmail.com>
2022-12-16 00:16:04 +0100

    chart improv

--- pages/graph/121122-tw-household-changes.js
+++ pages/graph/121122-tw-household-changes.js
@@ -25,6 +25,13 @@ export const options = {
   responsive: true,
   maintainAspectRatio: false,
   plugins: {
+    title: {
+      display: true,
+      text: '台灣的戶數淨變化與住宅存量、新建餘屋數變化比較',
+      font: {
+        size: 20
+      }
+    },
     legend: {
       position: 'bottom'
     }
@@ -67,24 +74,24 @@ export const data = {
     {
       label: '戶數淨變化（2021年後為國發會推估）',
       data: [null, '120737', '128671', '99828', '96439', '86279', '92405', '87617', '85477', '98268', '82167', '88205', '83979', '86811', '80909', '78641', '76428', '68888', '64553', '64515', '59634', '52828', '52560', '44789', '39590', '41192', '33155', '25966', '20916', '16208', '7678', '4076', '-4686', '-18319', '-16639', '-25055', null],
-      borderColor: '#B5917375',
-      backgroundColor: '#B59173',
+      borderColor: '#6929c475',
+      backgroundColor: '#6929c4',
       hidden: false,
       yAxisID: 'y'
     },
     {
       label: '全國住宅存量變化',
       data: [null, '68507', '91002', '84923', '90256', '84773', '108950', '93218', '138473', '90445', '97128', '101239', null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null],
-      borderColor: '#3E74C475',
-      backgroundColor: '#3E74C4',
+      borderColor: '#1192e875',
+      backgroundColor: '#1192e8',
       hidden: false,
       yAxisID: 'y'
     },
     {
       label: '新建餘屋數',
       data: [null, '24397', '27287', '34223', '45144', '63340', '75915', '78715', '78687', '77374', '73679', '74927', null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null],
-      borderColor: '#CB554D75',
-      backgroundColor: '#CB554D',
+      borderColor: '#005d5d75',
+      backgroundColor: '#005d5d',
       hidden: false,
       yAxisID: 'y'
     }
