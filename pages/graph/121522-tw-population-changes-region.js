10c8688f5ff54f6c98485325e085a0419aef84ad
imyentsen <imyentsen@gmail.com>
2022-12-16 00:44:55 +0100

    chart improv

--- pages/graph/121522-tw-population-changes-region.js
+++ pages/graph/121522-tw-population-changes-region.js
@@ -37,7 +37,10 @@ export const options = {
       text: '資料來源：國發會委託政大社會系《我國區域層級之家户推計模擬》'
     },
     legend: {
-      position: 'bottom'
+      position: 'bottom',
+      font: {
+        size: 16
+      }
     }
   },
   scales: {
