79770ba042a694d2be40372506fd03a6aa2b322d
imyentsen <imyentsen@gmail.com>
2022-12-16 00:25:53 +0100

    chart improv

--- pages/graph/121522-tw-population-changes-region.js
+++ pages/graph/121522-tw-population-changes-region.js
@@ -3,17 +3,19 @@ import {
   Chart as ChartJS,
   CategoryScale,
   LinearScale,
-  BarElement,
+  PointElement,
+  LineElement,
   Title,
   Tooltip,
   Legend
 } from 'chart.js'
-import { Bar } from 'react-chartjs-2'
+import { Line } from 'react-chartjs-2'
 
 ChartJS.register(
   CategoryScale,
   LinearScale,
-  BarElement,
+  PointElement,
+  LineElement,
   Title,
   Tooltip,
   Legend
@@ -57,7 +59,7 @@ export const data = {
     {
       label: '全國',
       data: ['4.69', '3.58', '2.39', '1.05', '-0.61'],
-      borderColor: 'rgba(0, 0, 0, 0)',
+      borderColor: '#6929c475',
       backgroundColor: '#6929c4',
       hidden: false,
       yAxisID: 'y'
@@ -65,7 +67,7 @@ export const data = {
     {
       label: '臺北市',
       data: ['3.01', '1.58', '0.25', '-1.86', '-4.42'],
-      borderColor: 'rgba(0, 0, 0, 0)',
+      borderColor: '#1192e875',
       backgroundColor: '#1192e8',
       hidden: false,
       pointStyle: 'rect',
@@ -75,7 +77,7 @@ export const data = {
     {
       label: '新北市',
       data: ['6.35', '5.72', '4.76', '3.55', '2.08'],
-      borderColor: 'rgba(0, 0, 0, 0)',
+      borderColor: '#005d5d75',
       backgroundColor: '#005d5d',
       hidden: false,
       pointStyle: 'rect',
@@ -85,7 +87,7 @@ export const data = {
     {
       label: '桃園市',
       data: ['8.72', '6.66', '4.68', '3.03', '0.98'],
-      borderColor: 'rgba(0, 0, 0, 0)',
+      borderColor: '#9f185375',
       backgroundColor: '#9f1853',
       hidden: false,
       pointStyle: 'rect',
@@ -95,7 +97,7 @@ export const data = {
     {
       label: '臺中市',
       data: ['6.41', '5.12', '3.69', '2.26', '0.69'],
-      borderColor: 'rgba(0, 0, 0, 0)',
+      borderColor: '#fa4d5675',
       backgroundColor: '#fa4d56',
       hidden: false,
       pointStyle: 'rect',
@@ -105,7 +107,7 @@ export const data = {
     {
       label: '臺南市',
       data: ['3.38', '2.41', '1.28', '0.04', '-1.51'],
-      borderColor: 'rgba(0, 0, 0, 0)',
+      borderColor: '#57040875',
       backgroundColor: '#570408',
       hidden: false,
       pointStyle: 'rect',
@@ -115,7 +117,7 @@ export const data = {
     {
       label: '高雄市',
       data: ['3.15', '2.06', '0.75', '-0.68', '-2.34'],
-      borderColor: 'rgba(0, 0, 0, 0)',
+      borderColor: '#19803875',
       backgroundColor: '#198038',
       hidden: false,
       pointStyle: 'rect',
@@ -125,7 +127,7 @@ export const data = {
     {
       label: '非六都',
       data: ['3.41', '2.33', '1.35', '0.28', '-1.18'],
-      borderColor: 'rgba(0, 0, 0, 0)',
+      borderColor: '#002d9c75',
       backgroundColor: '#002d9c',
       hidden: false,
       yAxisID: 'y'
@@ -136,7 +138,7 @@ export const data = {
 function GRAPH () {
   return (
   <div className="my-auto chart-wrapper bg-white">
-    <Bar options={options} data={data} height={500}/>
+    <Line options={options} data={data} height={500}/>
   </div>
   )
 }
