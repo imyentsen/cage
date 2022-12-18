a443d8b3dd9f015310ccd22ba3cbbf6e1d9a1383
imyentsen <imyentsen@gmail.com>
2022-12-18 01:12:01 +0100

    counterfactual

--- pages/graph/121822-counterfactual.js
+++ pages/graph/121822-counterfactual.js
@@ -61,16 +61,37 @@ export const options = {
   }
 }
 
-const labels = ['0（現況）', '1000平方公里', '2000平方公里', '3000平方公里', '日本', '新加坡', '荷蘭']
+const labels = ['台灣', '日本', '新加坡', '荷蘭']
 
 export const data = {
   labels,
   datasets: [
     {
-      label: '建地佔平原面積比',
-      data: [40.36, 50.90, 61.44, 82.51, 48.90, 75.25, 18.85],
+      label: '現況',
+      data: [40.36, 48.90, 75.25, 18.85],
       borderColor: 'rgba(0, 0, 0, 0)',
-      backgroundColor: '#009d9a',
+      backgroundColor: '#6929c4',
+      yAxisID: 'y'
+    },
+    {
+      label: '釋放1000km²',
+      data: [50.90],
+      borderColor: 'rgba(0, 0, 0, 0)',
+      backgroundColor: '#1192e8',
+      yAxisID: 'y'
+    },
+    {
+      label: '釋放2000km²',
+      data: [61.44],
+      borderColor: 'rgba(0, 0, 0, 0)',
+      backgroundColor: '#005d5d',
+      yAxisID: 'y'
+    },
+    {
+      label: '釋放3000km²',
+      data: [82.51],
+      borderColor: 'rgba(0, 0, 0, 0)',
+      backgroundColor: '#9f1853',
       yAxisID: 'y'
     }
   ]
