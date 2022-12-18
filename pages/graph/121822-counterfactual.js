c4b48b64a8d72fe622f30344a83888719fbba427
imyentsen <imyentsen@gmail.com>
2022-12-18 01:28:06 +0100

    counterfactual

--- pages/graph/121822-counterfactual.js
+++ pages/graph/121822-counterfactual.js
@@ -22,26 +22,9 @@ ChartJS.register(
 export const options = {
   responsive: true,
   maintainAspectRatio: false,
-  plugins: {
-    legend: {
-      position: 'bottom',
-      labels: {
-        font: {
-          size: 16
-        }
-      }
-    }
-  },
   scales: {
     x: {
       ticks: {
-        font: {
-          size: 16
-        }
-      },
-      title: {
-        display: true,
-        text: '釋放農地面積',
         font: {
           size: 16
         }
@@ -55,43 +38,22 @@ export const options = {
       },
       title: {
         display: true,
-        text: '每平方公里建地人口密度'
+        text: '百分比'
       }
     }
   }
 }
 
-const labels = ['台灣', '日本', '新加坡', '荷蘭']
+const labels = ['台灣', '釋放1000km²', '釋放2000km²', '釋放3000km²', '日本', '新加坡', '荷蘭']
 
 export const data = {
   labels,
   datasets: [
     {
       label: '現況',
-      data: [40.36, 48.90, 75.25, 18.85],
+      data: [40.36, 50.90, 61.44, 82.51, 48.90, 75.25, 18.85],
       borderColor: 'rgba(0, 0, 0, 0)',
-      backgroundColor: '#6929c4',
-      yAxisID: 'y'
-    },
-    {
-      label: '釋放1000km²',
-      data: [50.90],
-      borderColor: 'rgba(0, 0, 0, 0)',
-      backgroundColor: '#1192e8',
-      yAxisID: 'y'
-    },
-    {
-      label: '釋放2000km²',
-      data: [61.44],
-      borderColor: 'rgba(0, 0, 0, 0)',
-      backgroundColor: '#005d5d',
-      yAxisID: 'y'
-    },
-    {
-      label: '釋放3000km²',
-      data: [82.51],
-      borderColor: 'rgba(0, 0, 0, 0)',
-      backgroundColor: '#9f1853',
+      backgroundColor: '#009d9a',
       yAxisID: 'y'
     }
   ]
